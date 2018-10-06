76f69cb8a3d16954e30535030125937858ecacba
Kristjan Korjus <korjus@gmail.com>
2018-10-06 11:14:48 +0800

    tiny change to hello world

--- python2/helloworld.py
+++ python2/helloworld.py
@@ -1,5 +1,9 @@
-print "Dear Mom, </br></br>"
+print "Dear Mom,</br></br>"
 print "Look, I created this web site! I'm so happy. And if it gets famous, I can improve it later and make it even better.</br></br>"
-print "Also, it takes me just two lines of code in a programming language called Python to write 'Hellow world!' thousand times! Here it is: </br>"
+print "Also, it takes me just two lines of code in a programming language called Python to write 'Hello world!' thousand times! Here it is:</br></br>"
+
 for i in range(1000):
    print "Hello world!</br>"
+
+print "</br> With love,</br>"
+print "Dummy Hacker"

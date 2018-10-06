b5c7c55fa247fa601e805058c15ddd016c344882
Kristjan Korjus <korjus@gmail.com>
2018-10-06 11:08:07 +0800

    make hello world more friendly

--- python2/helloworld.py
+++ python2/helloworld.py
@@ -1,2 +1,5 @@
-for i in range(100):
+print "Dear Mom, </br></br>"
+print "Look, I created this web site! I'm so happy. And if it gets famous, I can improve it later and make it even better.</br></br>"
+print "Also, it takes me just two lines of code in a programming language called Python to write 'Hellow world!' thousand times! Here it is: </br>"
+for i in range(1000):
    print "Hello world!</br>"
